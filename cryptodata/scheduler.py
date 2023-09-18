d6330be60546fbdb8894c9a55360ea1755d9d737
Oleg Pak <pak.o99@mail.ru>
2023-09-18 05:30:39 +0300

    fixed scheduler

--- cryptodata/scheduler.py
+++ cryptodata/scheduler.py
@@ -1,8 +1,8 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .views import fetch_cryptocurrency_data
+from .views import fetch_cryptocurrency_data, f20
 
 def schedule_cryptocurrency_data_update():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(fetch_cryptocurrency_data(1000), 'interval', hours=1)
-    scheduler.add_job(fetch_cryptocurrency_data(200), 'interval', minutes=6)
+    scheduler.add_job(lambda: fetch_cryptocurrency_data(1000), 'interval', hours=1)
+    scheduler.add_job(lambda: fetch_cryptocurrency_data(200), 'interval', minutes=6)
     scheduler.start()

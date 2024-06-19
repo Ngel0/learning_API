b08e6412c93aec763d04937e91c989d43c9a8beb
Oleg Pak <pak.o99@mail.ru>
2024-06-19 17:18:17 +0300

    fixed old code for api class and added comments

--- cryptodata/views.py
+++ cryptodata/views.py
@@ -1,54 +1,53 @@
 import requests
-from .models import Cryptocurrency
-from favourites.models import Favourite
-from django.views.generic import ListView
 import os
+
+from django.views.generic import ListView
 from django.core.paginator import Paginator
+
+from favourites.models import Favourite
+from .models import Cryptocurrency
 
 
 # Class for handling API from CoinMarketCap
-# Not sure which ones will use yet
+# https://coinmarketcap.com/api/documentation/v1/
 class CoinMarketCap:
-    # https://coinmarketcap.com/api/documentation/v1/
+    # initializing base url and headers for all api calls
     def __init__(self, token) -> None:
         self.api_url = 'https://pro-api.coinmarketcap.com'
-        self.headers = {'Accepts': 'application/json',
+        self.headers = {'Accept': 'application/json',
+                        'Accept - Encoding': 'deflate, gzip',
                         'X-CMC_PRO_API_KEY': token, }
         self.session = requests.Session()
         self.session.headers.update(self.headers)
 
-    def get_all_coins(self):  # definitely need to change sth
+    # unique CoinMarketCap ID mapping
+    def get_all_coins_mapping(self):
         url = self.api_url + '/v1/cryptocurrency/map'
         parameters = {'limit': '10'}
         response = self.session.get(url, params=parameters)
         if response.status_code == 200:
-            data = response.json()['data']  # else raise error ?
-        return data
+            return response.json()['data']
+        else:
+            response.raise_for_status()
 
+    # latest market data for limit number of coins
     def get_latest_data(self, limit=100):
         url = self.api_url + '/v1/cryptocurrency/listings/latest'
         parameters = {'limit': limit}
         response = self.session.get(url, params=parameters)
         if response.status_code == 200:
-            data = response.json()['data']
-        return data
-
-    def get_price_symbol(self, symbol):
-        url = self.api_url + '/v2/cryptocurrency/quotes/latest'
-        parameters = {'symbol': symbol}
-        response = self.session.get(url, params=parameters)
-        if response.status_code == 200:
-            data = response.json()['data']
-        return data
+            return response.json()['data']
+        else:
+            response.raise_for_status()
 
 
-# Scheduled api call
+# function fetches and saves limit number of coin data for Scheduled api calls
 def fetch_cryptocurrency_data(limit):
-    api_key = os.environ['API_KEY']
+    api_key = os.getenv('API_KEY')
     cryptocurrencies = CoinMarketCap(api_key).get_latest_data(limit)
     for coin in cryptocurrencies:
         Cryptocurrency.objects.update_or_create(
-            symbol=coin['symbol'],
+            id=coin['id'],
             defaults={
                 'rank': coin['cmc_rank'],
                 'name': coin['name'],
@@ -63,6 +62,7 @@ def fetch_cryptocurrency_data(limit):
         )
 
 
+# ListView for displaying all coins
 class CoinsListView(ListView):
     model = Cryptocurrency
     context_object_name = 'cryptocurrencies'
